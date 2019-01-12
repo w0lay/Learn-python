84f3d09869144e5609e832e4cef65e952a5a6f6a
w0lay <46260479+w0lay@users.noreply.github.com>
2019-01-12 20:16:15 +0800

    Initial commit

---
+++ 1.2/test1.2.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+
+import math
+
+x = int(input('请输入一个数字：'))
+
+print(math.ceil(x))
+print(math.floor(x))
+print(math.fabs(x))
+print(math.factorial(x))
+#print(math.hypot(x,y))
+print(math.sqrt(x))
+#print(math.pow(x))
+print(math.log(x))
+print(math.log10(x))
+print(math.trunc(x))
+print(math.isnan(x))
+print(math.degrees(x))
+print(math.radians(x))
